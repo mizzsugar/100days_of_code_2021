cafac81d69daba087c9e02735815fbe93af2ced5
Mizuki Sato <mizki.sato.0025@gmail.com>
2021-11-03 16:19:32 +0900

    day7 rotate-array

--- src/day7.rs
+++ src/day7.rs
@@ -1,32 +1,32 @@
-// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
+// https://leetcode.com/problems/rotate-array
 
-pub fn max_profit(prices: Vec<i32>) -> i32 {
-    let mut i = 0;
-    let mut valley = prices[0];
-    let mut  peak = prices[0];
-    let mut  maxprofit = 0;
-    while i < prices.len() - 1 {
-        while i < prices.len() - 1 && prices[i] >= prices[i + 1] {
-            i += 1;
-        }
-        valley = prices[i];
-        while i < prices.len() - 1 && prices[i] <= prices[i + 1] {
-            i += 1;
-        }
-        peak = prices[i];
-        maxprofit += peak - valley;
-    }
-    maxprofit
+fn rotate(nums: &mut Vec<i32>, k: i32) {
+    let i = k as usize % nums.len();
+    reverse(nums, 0, nums.len() - 1);
+    reverse(nums, 0, i - 1);
+    reverse(nums, i, nums.len() - 1);
 }
 
+fn reverse(nums: &mut Vec<i32>, start: usize, end: usize) {
+    let mut start_ = start.clone();
+    let mut end_ = end.clone();
+    while start_ < end_ {
+        let tmp = nums[start_];
+        nums[start_] = nums[end_];
+        nums[end_] = tmp;
+        start_ += 1;
+        end_ -= 1;
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::max_profit;
+    use super::rotate;
     #[test]
-    fn test_max_profit() {
-        let actual = max_profit(vec![7, 1, 3, 5, 3, 6, 4]);
-        let expected: i32 = 7;
-        assert_eq!(actual, expected);
+    fn test_rotate() {
+        let mut v: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7];
+        let expected: Vec<i32> = vec![5, 6, 7, 1, 2, 3, 4];
+        rotate(&mut v, 3);
+        assert_eq!(v, expected);
     }
 }
